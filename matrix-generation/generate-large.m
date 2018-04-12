4cf86ca2455d9c350a12fe9a5ef8f1329c303f8e
Michael Lass <michael.lass@uni-paderborn.de>
2018-04-12 15:44:59 +0200

    Indentation

--- matrix-generation/generate-large.m
+++ matrix-generation/generate-large.m
@@ -25,11 +25,11 @@ cond = 2;
 
 for rep = 1:3;
   for exp = 11:1:18;
-          size = 2^exp;
-          density = 1000000;
-          prefix = strcat('sprandsym-s', num2str(size), '-d', num2str(density), '-c', num2str(cond), '-n', num2str(rep));
-          A = sprandsym(size, density/100000000, 1/cond, 2);
-          [val,row_ind,col_ptr] = extractCSC(A, prefix);
+    size = 2^exp;
+    density = 1000000;
+    prefix = strcat('sprandsym-s', num2str(size), '-d', num2str(density), '-c', num2str(cond), '-n', num2str(rep));
+    A = sprandsym(size, density/100000000, 1/cond, 2);
+    [val,row_ind,col_ptr] = extractCSC(A, prefix);
   end;
 
   for exp = 10:1:20;
